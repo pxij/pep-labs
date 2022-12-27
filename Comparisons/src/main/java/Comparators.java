49ae66ac397cb14e90d081c741c2f7dc21506e88
pxij <dan.rennier@gmail.com>
2022-12-27 21:45:22 +0000

    Saved progress on Comparisons 2022-12-27 21:45:22 +0000

--- Comparisons/src/main/java/Comparators.java
+++ Comparisons/src/main/java/Comparators.java
@@ -1,3 +1,4 @@
+import javax.lang.model.util.ElementScanner6;
 
 /**
  * If we want to make programs that "do something", we're going to need to create instructions that change
@@ -14,8 +15,12 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        if (a==b) {           
+            return true; 
+        }
+            return false;
     }
+    
 
     /**
      * @param a the first number to be compared.
@@ -23,6 +28,9 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
+        if (a>b) {   
+            return true;  
+        }
         return false;
     }
     /**
@@ -31,6 +39,9 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
+        if (a<b) {   
+            return true;  
+        }
         return false;
     }
     /**
@@ -39,6 +50,9 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
+        if (a>=b) {   
+            return true;  
+        }
         return false;
     }
     /**
@@ -47,6 +61,9 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
+        if (a<=b) {   
+            return true;  
+        }
         return false;
     }
 
@@ -56,6 +73,9 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
+        if (a!=b) {   
+            return true;  
+        }
         return false;
     }
 }
