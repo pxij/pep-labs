9d7369be776fa0e7966fa299f034e734567fb265
pxij <dan.rennier@gmail.com>
2022-12-28 21:28:11 +0000

    Saved progress on IntroToOOP 2022-12-28 21:28:11 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -45,7 +45,11 @@ public class Bird {
      *  * greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        String C = "Chirp";
+        String T = "Tweet";
+        if (ageMonths < 3) { 
+            return T;
+        }
+        return C;
     }
-
 }
