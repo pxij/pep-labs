2b11d887ea678188376051ab1758c10d936bbfb5
pxij <dan.rennier@gmail.com>
2022-12-29 23:34:00 +0000

    Saved progress on ReturnArrayContents 2022-12-29 23:34:00 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -28,6 +28,10 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+            String arrString = "";
+            for (int i = 0; i < nums.length; i++){
+                arrString = arrString + nums[i] + " ";
+            }           
+            return arrString;
+        }
     }
-}
